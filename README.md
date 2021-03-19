b6913358ae41633ae1e488c35296804e5748c9d5
irmerk <jolenelanglinais@gmail.com>
2021-03-19 11:02:56 -0400

    wip: investigate node http

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # Structure
 
-- Analysis
+- Analysis & Comparison
   - [`got`](https://github.com/sindresorhus/got#readme)
   - [`request`](https://github.com/request/request#readme)
     - [`request-promise`](https://github.com/request/request-promise#readme)
     - [`promisify`](https://github.com/dpw/promisify#readme)
   - [`axios`](https://github.com/axios/axios#table-of-contents)
   - [`fetch`](https://github.com/andris9/fetch#fetch)
-- Comparison
+  - Code snippets... 
+  - Syntax, readability
+  - Structure of responses
+  - Method for catching errors
+  - Structure of errors
 - Suggestion
-- Recommendation
+  - GOT
+  - Why?
+- Recommendation (Inside Express.js)
+  - Migration reference
+
+## Error Types
+
+Exception
+  Error in JS
+
+Network Error
+  F
+
+HTTP Error
+  Nothing to do with JS, receives response and status code
+
+LOOK INTO: https://nodejs.org/api/http.html
--- index.js
+++ index.js
@@ -6,40 +6,60 @@ const promisify = require('promisify');
 const requestPromise = require('request-promise');
 
 const sadUrl = 'https://svc-helper-api-wrong.clause.io/multipart-upload';
+const grumpyUrl = `https://svc-helper-api.clause.io/errors/500`
 const happyUrl = 'https://svc-helper-api.clause.io/';
 
-const runRequest = async (url) => {
+const runRequest = (url) => {
   console.log('---------------------------> START request');
-  try {
-    // await doesn't make sense for request...
-    await request(url, (error, response, body) => {
-	    console.log('REQUEST error:', error);
-	    console.log('REQUEST statusCode:', response?.statusCode);
-	    console.log('REQUEST body:', body);
-      console.log('REQUEST DONE');
-    });
-  } catch (error) {
-    console.log('REQUEST THROWS ', error);
-  }
+
+  request(happyUrl, function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
   console.log('---------------------------> FINISH request');
 }
 
 const runGot = async (url) => {
-  console.log('---------------------------> START got');
+  console.log('---------------------------> START got with url ', url);
   try {
-    const response = await got.get(url)
-    console.log('GOT statusCode:', response?.statusCode);
+    const response = await got.get(url, { throwHttpErrors: false })
+    // console.log('GOT statusCode:', response?.statusCode);
+    // console.log('GOT body:', response?.body);
+    console.log('GOT response:', response);
     console.log('GOT DONE');
   } catch (error) {
     console.log('GOT THROWS ', error);
   }
-  console.log('---------------------------> FINISH got');
+  console.log('---------------------------> FINISH got with url ', url);
 }
 
-const run = async () => {
-  await runGot(happyUrl);
-  // await runGot(sadUrl);
+const runCallbackLibraries = () => {
+  // request
+  // equivalent in got
+
   // await runRequest(sadUrl);
+};
+
+const runPromiseLibraries = async () => {
+  // request-promise, promisifiy, etc.
+  // equivalent in got
+  // await runGot(happyUrl);
+  await runGot(grumpyUrl);
+
+};
+
+const run = () => {
+
+  // runCallbackLibraries();
+  runPromiseLibraries();
+
+
+  /*
+    * Set up calls so they all 'do the same thing'
+    * Collect response examples
+    * Collect error examples
+  */
 }
 
 run();
